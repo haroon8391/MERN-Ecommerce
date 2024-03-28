7dcb5f1add97203da7e8d4fd5794717e4df5bd2c
Muhammad Haroon Ali <111272010+haroon8391@users.noreply.github.com>
2024-03-28 15:39:01 +0500

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -15,6 +15,7 @@ app.use(cors(
 ))
 app.use(express.json());
 app.use("/api/auth", router)
+app.get("/", (req, res) => res.send("Express on Vercel"));
 
 
 app.post("/create-checkout-session", async (req, res) => {
